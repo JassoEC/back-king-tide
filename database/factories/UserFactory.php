668857672678b325ca5ec8db6269533bbc407a20
Emanuel Campos Jasso <isw.ecamposj@gmail.com>
2022-01-30 00:11:16 -0600

    se agregaron los campos faltantes al UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,7 +3,6 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 
 class UserFactory extends Factory
 {
@@ -15,11 +14,11 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'name'      => $this->faker->name(),
+            'last_name' => $this->faker->lastName(),
+            'sur_name'  => $this->faker->lastName(),
+            'birthday'  => $this->faker->dateTimeBetween('1980-01-01', '2000-12-31'),
+            'rfc'       => $this->faker->regexify('[A-Za-z0-9]{18}'),
         ];
     }
 
