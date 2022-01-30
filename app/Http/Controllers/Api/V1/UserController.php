0742281a4aa9219f95ca11d3cef82b68f47de1d5
Emanuel Campos Jasso <isw.ecamposj@gmail.com>
2022-01-29 23:56:06 -0600

    se creo el Api\V1\UserController

---
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\V1\UserRequest;
+use App\Http\Resources\V1\UserResource;
+use App\Models\User;
+use App\Repositories\UserRepository;
+use App\Services\V1\UserService;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(UserRepository $repository)
+    {
+        $data = $repository->getPaginatedData();
+        return UserResource::collection($data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(UserService $service, UserRequest $request)
+    {
+        $user = $service->storeUser($request);
+        return new UserResource($user);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        return new UserResource($user);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UserService $service, UserRequest $request, User $user)
+    {
+        $user = $service->updateUser($request, $user);
+        return new UserResource($user);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return new UserResource($user);
+    }
+}
