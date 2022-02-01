2e6e4444394b915dfe692b7af17ab62736ab4c07
Emanuel Campos Jasso <isw.ecamposj@gmail.com>
2022-01-31 22:27:54 -0600

    finalizacion de metodos de manipulacion de datos de usuario

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -49,7 +49,7 @@ class UserController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Http\Requests\V1\UserRequest
      * @param  \App\Models\User  $user
      * @return \Illuminate\Http\Response
      */
