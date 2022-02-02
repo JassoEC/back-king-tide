af83dd5524c37a59a251d6b022e41461242396c1
Emanuel Campos Jasso <34528183+JassoEC@users.noreply.github.com>
2022-02-02 12:39:17 -0600

    Update UserResource.php
    Se agrego el atributo id en el UserResource

--- app/Http/Resources/V1/UserResource.php
+++ app/Http/Resources/V1/UserResource.php
@@ -15,6 +15,7 @@ class UserResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id'                 => $this->id,
             'name'               => $this->name,
             'lastName'           => $this->last_name,
             'surName'            => $this->sur_name,
